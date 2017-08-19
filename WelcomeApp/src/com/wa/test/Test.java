3233990e2b901de3b5ac303b9f1231fe05db0e7c
gorikela <shekarg71@yahoo.com>
2017-08-19 16:10:14 +0530

    added additinal String
    Change-Id: I65762d70c1f9a167e647de998f0ef784139fcaec

--- WelcomeApp/src/com/wa/test/Test.java
+++ WelcomeApp/src/com/wa/test/Test.java
@@ -2,7 +2,7 @@ package com.wa.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("Welcome first");
 		System.out.println("Second");
 	}
 }
